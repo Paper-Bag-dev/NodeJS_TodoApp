0be126176d333c5cd35a5478ab4cb26d859620ff
Vikalp Sharma <vikalpsh1234@gmail.com>
2023-09-26 11:56:34 +0530

    changed to new cors policy code

--- app.js
+++ app.js
@@ -16,11 +16,7 @@ config({
 // middleware to access/send json data through body
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods : ["GET", "POST", "PUT", "DELETE"],
-    credentials : true,
-}))
+app.use(cors());
 
 // Routes
 app.use("/api/v1/users" ,userRouter);
