bff6a887d83f8deea4564fe34eb1d6fa5f7dc66c
Vikalp Sharma <vikalpsh1234@gmail.com>
2023-10-02 17:43:57 +0530

    changed cors function to old args

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ config({
 // middleware to access/send json data through body
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 
 // Routes
 app.use("/api/v1/users" ,userRouter);
