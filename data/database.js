3e7bddaf75a1b7655e3548d4c624359c6d55669d
Vikalp Sharma <vikalpsh1234@gmail.com>
2023-10-12 20:16:56 +0530

    Rolled back

--- data/database.js
+++ data/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const ConnectDB = () => {
-    mongoose.createConnection(process.env.MONGO_URI, {dbName : "Todo_API"})
+    mongoose.connect(process.env.MONGO_URI, {dbName : "Todo_API"})
     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
