0cfb208002fe8d3ee05434a193e516bcf72ad376
Vikalp Sharma <vikalpsh1234@gmail.com>
2023-10-12 20:14:21 +0530

    Changed to createConnection

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "UPDATE"],
     credentials: true,
 }));
 
